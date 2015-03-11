93ae6bffeba124a59d6f3dc07c6513ad8e4fea92
a <alexanderm@web.de>
2015-03-11 08:02:22 +0100

    Removed auctex-init that interfered with company-math. Increased delay.

--- .emacs.d/init/company.el
+++ .emacs.d/init/company.el
@@ -6,5 +6,9 @@
 
 (add-to-list 'company-backends '(company-latex-commands company-math-symbols-latex company-math-symbols-unicode))
 
+;;(company-auctex-init)
+
+(setq company-idle-delay 3)
+
 (define-key evil-normal-state-map (kbd "C-<SPC>") 'company-complete)
 (define-key evil-insert-state-map (kbd "C-<SPC>") 'company-complete)
