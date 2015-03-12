b3c6a8be3a494a5b9c7938313caba23cb7b39399
a <alexanderm@web.de>
2015-03-12 08:23:57 +0100

    Added custom functions file.

---
+++ .emacs.d/init/functions.el
@@ -0,0 +1,12 @@
+;; ========================
+;; my own custom functions
+;; ========================
+
+(defun my/def-key-for-maps (key cmd maps)
+  "Define key binding for all given key-maps."
+  (mapcar (lambda (map) (define-key map key cmd)) maps))
+
+(defun aggressive-indent-if ()
+  "Activate aggressive indent mode unless the current major mode prevents it."
+  (unless (member (buffer-local-value 'major-mode (current-buffer)) '(python-mode org-mode))
+    (aggressive-indent-mode 1)))
