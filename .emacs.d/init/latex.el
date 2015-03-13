6d34fa8bbabbee9ce8ce42e28d9d01b3441b3d92
a <alexanderm@web.de>
2015-03-13 08:19:31 +0100

    Corrected TAB, added font key binds.

--- .emacs.d/init/latex.el
+++ .emacs.d/init/latex.el
@@ -32,22 +32,32 @@
 (setq font-latex-fontify-sectioning 'color)
 
 (evil-leader/set-key-for-mode 'latex-mode
-  "[TAB] e"   'LaTeX-environment
-  "[TAB] s"   'LaTeX-section
-  "[TAB] j"   'LaTeX-insert-item
-  "[TAB] ]"   'LaTeX-close-environment
-  "[TAB] f e" 'LaTeX-fill-environment
-  "[TAB] f r" 'LaTeX-fill-region
-  "[TAB] f s" 'LaTeX-fill-section
-  "[TAB] f f" 'LaTeX-fill-buffer
-  "[TAB] p e" 'preview-environment
-  "[TAB] p r" 'preview-region
-  "[TAB] p s" 'preview-section
-  "[TAB] p f" 'preview-buffer
-  "[TAB] p p" 'preview-at-point
-  "[TAB] P r" 'preview-clearout
-  "[TAB] P s" 'preview-clearout-section
-  "[TAB] P b" 'preview-clearout-buffer
-  "[TAB] P p" 'preview-clearout-at-point
-  "[TAB] P f" 'preview-clearout-document)
+  "<tab> e"   'LaTeX-environment
+  "<tab> s"   'LaTeX-section
+  "<tab> i"   'LaTeX-insert-item
+  "<tab> c"   'LaTeX-close-environment
+
+  "<tab> f e" 'LaTeX-fill-environment
+  "<tab> f r" 'LaTeX-fill-region
+  "<tab> f s" 'LaTeX-fill-section
+  "<tab> f f" 'LaTeX-fill-buffer
+
+  "<tab> o b" '(lambda () (interactive) (TeX-font nil 2) (evil-insert 1))
+  "<tab> o s" '(lambda () (interactive) (TeX-font nil 3) (evil-insert 1))
+  "<tab> o e" '(lambda () (interactive) (TeX-font nil 5) (evil-insert 1))
+  "<tab> o i" '(lambda () (interactive) (TeX-font nil 9) (evil-insert 1))
+  "<tab> o r" '(lambda () (interactive) (TeX-font nil 18) (evil-insert 1))
+  "<tab> o l" '(lambda () (interactive) (TeX-font nil 19) (evil-insert 1))
+  "<tab> o t" '(lambda () (interactive) (TeX-font nil 20) (evil-insert 1))
+
+  "<tab> p e" 'preview-environment
+  "<tab> p r" 'preview-region
+  "<tab> p s" 'preview-section
+  "<tab> p f" 'preview-buffer
+  "<tab> p p" 'preview-at-point
+  "<tab> P r" 'preview-clearout
+  "<tab> P s" 'preview-clearout-section
+  "<tab> P b" 'preview-clearout-buffer
+  "<tab> P p" 'preview-clearout-at-point
+  "<tab> P f" 'preview-clearout-document)
 
