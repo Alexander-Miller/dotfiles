541d4fb45f4ba70d713bba824683dc1a26b6987e
a <alexanderm@web.de>
2015-04-07 23:13:03 +0200

    Switched to with-eval-after-load.

--- .emacs.d/init/latex.el
+++ .emacs.d/init/latex.el
@@ -10,55 +10,56 @@
              '(setq reftex-plug-into-AUCTeX 1)
              '(latex-math-mode)))
 
-;; save style information when saving the buffer
-(setq TeX-auto-save 1)
+(with-eval-after-load "latex"
 
-;; parse file after loading if no style hook is found
-(setq TeX-parse-self 1)
+  ;; save style information when saving the buffer
+  (setq TeX-auto-save 1)
 
-;;AUCTeX multidoc awareness for \input
-(setq-default TeX-master nil)
+  ;; parse file after loading if no style hook is found
+  (setq TeX-parse-self 1)
 
-;; use PDF TeX as default executable
-(setq TeX-PDF-mode 1)
+  ;;AUCTeX multidoc awareness for \input
+  (setq-default TeX-master nil)
 
-;; do not ask for permission to start TeX
-(setq TeX-save-query nil)
+  ;; use PDF TeX as default executable
+  (setq TeX-PDF-mode 1)
 
-;;proper indentation of list items
-(setq LaTeX-item-indent 0)
+  ;; do not ask for permission to start TeX
+  (setq TeX-save-query nil)
 
-;; dont use different text sizes
-(setq font-latex-fontify-script nil)
-(setq font-latex-fontify-sectioning 'color)
+  ;;proper indentation of list items
+  (setq LaTeX-item-indent 0)
 
-(evil-leader/set-key-for-mode 'latex-mode
-  "<tab> e"   'LaTeX-environment
-  "<tab> s"   'LaTeX-section
-  "<tab> i"   'LaTeX-insert-item
-  "<tab> c"   'LaTeX-close-environment
+  ;; dont use different text sizes
+  (setq font-latex-fontify-script nil)
+  (setq font-latex-fontify-sectioning 'color)
 
-  "<tab> f e" 'LaTeX-fill-environment
-  "<tab> f r" 'LaTeX-fill-region
-  "<tab> f s" 'LaTeX-fill-section
-  "<tab> f f" 'LaTeX-fill-buffer
+  (evil-leader/set-key-for-mode 'latex-mode
+    "<tab> e"   'LaTeX-environment
+    "<tab> s"   'LaTeX-section
+    "<tab> i"   'LaTeX-insert-item
+    "<tab> c"   'LaTeX-close-environment
 
-  "<tab> o b" '(lambda () (interactive) (TeX-font nil 2) (evil-insert 1))
-  "<tab> o s" '(lambda () (interactive) (TeX-font nil 3) (evil-insert 1))
-  "<tab> o e" '(lambda () (interactive) (TeX-font nil 5) (evil-insert 1))
-  "<tab> o i" '(lambda () (interactive) (TeX-font nil 9) (evil-insert 1))
-  "<tab> o r" '(lambda () (interactive) (TeX-font nil 18) (evil-insert 1))
-  "<tab> o l" '(lambda () (interactive) (TeX-font nil 19) (evil-insert 1))
-  "<tab> o t" '(lambda () (interactive) (TeX-font nil 20) (evil-insert 1))
+    "<tab> f e" 'LaTeX-fill-environment
+    "<tab> f r" 'LaTeX-fill-region
+    "<tab> f s" 'LaTeX-fill-section
+    "<tab> f f" 'LaTeX-fill-buffer
 
-  "<tab> p e" 'preview-environment
-  "<tab> p r" 'preview-region
-  "<tab> p s" 'preview-section
-  "<tab> p f" 'preview-buffer
-  "<tab> p p" 'preview-at-point
-  "<tab> P r" 'preview-clearout
-  "<tab> P s" 'preview-clearout-section
-  "<tab> P b" 'preview-clearout-buffer
-  "<tab> P p" 'preview-clearout-at-point
-  "<tab> P f" 'preview-clearout-document)
+    "<tab> o b" '(lambda () (interactive) (TeX-font nil 2) (evil-insert 1))
+    "<tab> o s" '(lambda () (interactive) (TeX-font nil 3) (evil-insert 1))
+    "<tab> o e" '(lambda () (interactive) (TeX-font nil 5) (evil-insert 1))
+    "<tab> o i" '(lambda () (interactive) (TeX-font nil 9) (evil-insert 1))
+    "<tab> o r" '(lambda () (interactive) (TeX-font nil 18) (evil-insert 1))
+    "<tab> o l" '(lambda () (interactive) (TeX-font nil 19) (evil-insert 1))
+    "<tab> o t" '(lambda () (interactive) (TeX-font nil 20) (evil-insert 1))
 
+    "<tab> p e" 'preview-environment
+    "<tab> p r" 'preview-region
+    "<tab> p s" 'preview-section
+    "<tab> p f" 'preview-buffer
+    "<tab> p p" 'preview-at-point
+    "<tab> P r" 'preview-clearout
+    "<tab> P s" 'preview-clearout-section
+    "<tab> P b" 'preview-clearout-buffer
+    "<tab> P p" 'preview-clearout-at-point
+    "<tab> P f" 'preview-clearout-document))
