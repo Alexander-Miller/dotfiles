525cc27021c9f621111e2c12590a5dacebe9a3f0
a <alexanderm@web.de>
2015-05-15 17:03:18 +0200

    Use emacs state for shell, remove yas for proper tab completion.

--- .emacs.d/init/shell-cfg.el
+++ .emacs.d/init/shell-cfg.el
@@ -1,23 +1,24 @@
 ;;; shell-cfg.el --- emacs shell configuration
 
 ;;; Commentary:
-
 ;;; Code:
 
 (add-hook 'term-mode-hook
           '(lambda ()
              (setq-local company-backends '())
+             (yas-minor-mode 0)
              (nlinum-mode 0)
              (setq-local scroll-margin 0)))
 
 (global-set-key [f1] 'multi-term-dedicated-toggle)
 
 (with-eval-after-load "term"
+  (evil-set-initial-state 'term-mode 'emacs)
   (setq-default
    term-buffer-maximum-size                 10000
    term-suppress-hard-newline               nil
    multi-term-program                       "/usr/bin/fish"
-   multi-term-dedicated-buffer-name         "Dedicated Terminal"
+   multi-term-dedicated-buffer-name         "Dedicated Fish"
    multi-term-scroll-to-bottom-on-output    t
    multi-term-switch-after-close            nil
    multi-term-dedicated-window-height       33
