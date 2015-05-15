b30260d18455e86c0d99c4e32aa47e0e64fa6398
a <alexanderm@web.de>
2015-05-15 16:56:46 +0200

    Add jk movement to more keymaps.

--- .emacs.d/init/magit-cfg.el
+++ .emacs.d/init/magit-cfg.el
@@ -10,22 +10,17 @@
   (define-key magit-status-mode-map (kbd "C-v") 'set-mark-command)
 
   (my/def-key-for-maps
-   (kbd "j") 'next-line (list magit-status-mode-map magit-log-mode-map))
+   (kbd "j") 'next-line (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
   (my/def-key-for-maps
-   (kbd "k") 'previous-line (list magit-status-mode-map magit-log-mode-map))
-
-  (my/def-key-for-maps
-   (kbd "j") 'next-line (list magit-commit-mode-map))
-  (my/def-key-for-maps
-   (kbd "k") 'previous-line (list magit-commit-mode-map))
+   (kbd "k") 'previous-line (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
 
   (my/def-key-for-maps
    (kbd "C-k") 'magit-discard-item (list magit-status-mode-map))
 
   (my/def-key-for-maps
-   (kbd "M-j") 'magit-goto-next-sibling-section (list magit-status-mode-map))
+   (kbd "M-j") 'magit-goto-next-sibling-section (list magit-status-mode-map magit-diff-mode-map))
   (my/def-key-for-maps
-   (kbd "M-k") 'magit-goto-previous-sibling-section (list magit-status-mode-map)))
+   (kbd "M-k") 'magit-goto-previous-sibling-section (list magit-status-mode-map magit-diff-mode-map)))
 
 (provide 'magit-cfg)
 ;;; magit-cfg.el ends here
