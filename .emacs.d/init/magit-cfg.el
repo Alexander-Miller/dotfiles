2ed9765ee72224bfd1d5a125cca6806c8eeb216c
a <alexanderm@web.de>
2015-06-02 17:12:35 +0200

    Add faster scroll key & minor settings.

--- .emacs.d/init/magit-cfg.el
+++ .emacs.d/init/magit-cfg.el
@@ -7,12 +7,25 @@
 
 (with-eval-after-load 'magit
 
+  (setq-default
+   magit-highlight-trailing-whitespace t
+   magit-key-mode-show-usage           t
+   magit-revert-backup                 t
+   magit-revert-item-confirm           t
+   magit-stage-all-confirm             t
+   magit-use-overlays                  t)
+
   (define-key magit-status-mode-map (kbd "C-v") 'set-mark-command)
 
   (my/def-key-for-maps
    (kbd "j") 'next-line (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
   (my/def-key-for-maps
    (kbd "k") 'previous-line (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
+
+  (my/def-key-for-maps
+   (kbd "J") 'my/quick-forward (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
+  (my/def-key-for-maps
+   (kbd "K") 'my/quick-backward (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
 
   (my/def-key-for-maps
    (kbd "C-k") 'magit-discard-item (list magit-status-mode-map))
