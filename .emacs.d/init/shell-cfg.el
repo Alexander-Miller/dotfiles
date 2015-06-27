ac773d6d3c8715da3374f3e1be2ae7dde1656bf1
a <alexanderm@web.de>
2015-06-27 20:11:18 +0200

    Add kill word key binds.

--- .emacs.d/init/shell-cfg.el
+++ .emacs.d/init/shell-cfg.el
@@ -7,23 +7,33 @@
           '(lambda ()
              (setq-local company-backends '())
              (yas-minor-mode 0)
-             (nlinum-mode 0)
              (setq-local scroll-margin 0)))
 
 (global-set-key [f1] 'multi-term-dedicated-toggle)
 
 (with-eval-after-load "term"
+
   (evil-set-initial-state 'term-mode 'emacs)
+
+  (define-key term-raw-map (kbd "M-l") 'term-send-forward-word)
+  (define-key term-raw-map (kbd "M-h") 'term-send-backward-word)
+  (define-key term-raw-map (kbd "M-<backspace>") 'term-send-backward-kill-word)
+
   (setq-default
-   term-buffer-maximum-size                 10000
-   term-suppress-hard-newline               nil
-   multi-term-program                       "/usr/bin/fish"
-   multi-term-dedicated-buffer-name         "Dedicated Fish"
-   multi-term-scroll-to-bottom-on-output    t
-   multi-term-switch-after-close            nil
-   multi-term-dedicated-window-height       50
-   multi-term-dedicated-max-window-height   50
-   multi-term-dedicated-select-after-open-p t))
+   multi-term-buffer-name                           "Fish"
+   multi-term-dedicated-buffer-name                 "Dedicated Fish"
+   multi-term-dedicated-close-back-to-open-buffer-p t
+   multi-term-dedicated-max-window-height           50
+   multi-term-dedicated-select-after-open-p         t
+   multi-term-dedicated-skip-other-window-p         nil
+   multi-term-dedicated-window-height               50
+   multi-term-default-dir                           "~/"
+   multi-term-program                               "/usr/bin/fish"
+   multi-term-scroll-to-bottom-on-output            t
+   multi-term-try-create                            nil
+   multi-term-switch-after-close                    nil
+   term-buffer-maximum-size                         10000
+   term-suppress-hard-newline                       nil))
 
 (provide 'shell-cfg)
 ;;; shell-cfg.el ends here
