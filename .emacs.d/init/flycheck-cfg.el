66c0a4bd3ebee1ed59b9eacfbd034abd4f85592e
a <alexanderm@web.de>
2015-06-27 20:49:50 +0200

    Use flycheck in all prog modes.

--- .emacs.d/init/flycheck-cfg.el
+++ .emacs.d/init/flycheck-cfg.el
@@ -3,14 +3,16 @@
 ;;; Commentary:
 ;;; Code:
 
-(with-eval-after-load "flycheck"
+(add-hook prog-mode-hook '(lambda () (flycheck-mode t)))
 
+(with-eval-after-load "flycheck"
 
   (defun flycheck-pos-tip-error-messages (errors)
     "Display flycheck's ERRORS with pos-tip."
     (-when-let (messages (-keep #'flycheck-error-message errors))
       (pos-tip-show (mapconcat 'identity messages "\n\n") nil nil nil -1)))
 
+  (setq-default flycheck-keymap-prefix (kbd "C-c f"))
   (setq-default
    flycheck-idle-change-delay          4
    flycheck-display-errors-function    #'flycheck-pos-tip-error-messages
@@ -18,7 +20,6 @@
    flycheck-check-syntax-automatically '(mode-enabled save idle-change))
 
   (define-key flycheck-mode-map flycheck-keymap-prefix nil)
-  (setq-default flycheck-keymap-prefix (kbd "C-c f"))
   (define-key flycheck-mode-map flycheck-keymap-prefix flycheck-command-map))
 
 (provide 'flycheck-cfg)
