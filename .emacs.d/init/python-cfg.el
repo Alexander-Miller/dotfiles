e4b3a15067ba4557876c248eff707d3386da9303
a <alexanderm@web.de>
2015-06-28 00:29:07 +0200

    Refactor hooks.

--- .emacs.d/init/python-cfg.el
+++ .emacs.d/init/python-cfg.el
@@ -3,9 +3,16 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-hook 'python-mode-hook
-          '(lambda () (setq-local company-backends
-                             '((elpy-company-backend company-yasnippet company-files company-dabbrev-code)))))
+(defun my/python-mode-hook ()
+  "Python mode hook."
+  (setq-local company-backends '((elpy-company-backend company-yasnippet company-files company-dabbrev-code))))
+
+(defun my/inferior-python-mode-hook ()
+  "Inferior Python mode hook."
+  (setq-local company-backends '((company-capf))))
+
+(add-hook 'python-mode-hook          #'my/python-mode-hook)
+(add-hook 'inferior-python-mode-hook #'my/inferior-python-mode-hook)
 
 (with-eval-after-load 'python-mode
   (setq-default elpy-rpc-backend "jedi")
