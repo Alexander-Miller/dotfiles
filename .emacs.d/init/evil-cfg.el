0da69f5ea023f0b2a66152c40bc5d66ebc6d5450
a <alexanderm@web.de>
2015-06-28 12:05:18 +0200

    Make sure evil visual exit reenables hl-line only if it was turned on previously.

--- .emacs.d/init/evil-cfg.el
+++ .emacs.d/init/evil-cfg.el
@@ -18,8 +18,24 @@
  evil-repeat-move-cursor 0
  evil-find-skip-newlines t)
 
-(add-hook 'evil-visual-state-entry-hook (lambda () (setq-local global-hl-line-mode nil)))
-(add-hook 'evil-visual-state-exit-hook  (lambda () (setq-local global-hl-line-mode t)))
+(defvar hl-line-was-on nil)
+
+(defun visual-entry-hook ()
+  "Evil visual state entry hook."
+  (if global-hl-line-mode
+      (progn
+        (setq-local hl-line-was-on t)
+        (setq-local global-hl-line-mode nil))))
+
+(defun visual-exit-hook ()
+  "Evil visual state exit hook."
+  (if hl-line-was-on
+      (progn
+        (setq-local hl-line-was-on nil)
+        (setq-local global-hl-line-mode t))))
+
+(add-hook 'evil-visual-state-entry-hook #'visual-entry-hook)
+(add-hook 'evil-visual-state-exit-hook  #'visual-exit-hook)
 
 (provide 'evil-cfg)
 ;;; evil-cfg.el ends here
