6dd4e90c6360e075ad8be3fcde7ec67e269751ed
a <alexanderm@web.de>
2015-06-30 01:07:59 +0200

    Expand settings and keybinds.

--- .emacs.d/init/smartparens-cfg.el
+++ .emacs.d/init/smartparens-cfg.el
@@ -11,14 +11,30 @@
 (show-smartparens-global-mode t)
 
 (setq-default
- sp-show-pair-from-inside t
- sp-show-pair-delay       0.1)
+ sp-autodelete-pair         t
+ sp-autodelete-wrap         t
+ sp-autodelete-closing-pair t
+ sp-autodelete-opening-pair t
+ sp-autoinsert-pair         t
+ sp-autowrap-region         t
+ sp-show-pair-from-inside   t
+ sp-show-pair-delay         0.1
+ sp-use-subword             t)
+
+(define-key smartparens-strict-mode-map [remap sp-delete-char] 'delete-char)
+
+(define-key evil-visual-state-map (kbd "(")  (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "(")))
+(define-key evil-visual-state-map (kbd "[")  (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "[")))
+(define-key evil-visual-state-map (kbd "{")  (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "{")))
+(define-key evil-visual-state-map (kbd "\"") (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "\"")))
 
 (evil-leader/set-key
   "s f" 'sp-forward-slurp-sexp
   "s F" 'sp-forward-barf-sexp
   "s b" 'sp-backward-slurp-sexp
-  "s B" 'sp-backward-barf-sexp)
+  "s B" 'sp-backward-barf-sexp
+  "s u" 'sp-unwrap-sexp
+  "s r" 'sp-rewrap-sexp)
 
 (provide 'smartparens-cfg)
 ;;; smartparens-cfg.el ends here
