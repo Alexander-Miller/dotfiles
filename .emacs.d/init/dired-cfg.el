48e67ca006fcceb22356d4227779d87cd991546e
a <alexanderm@web.de>
2015-07-14 17:26:04 +0200

    Change rainbow colors.

--- .emacs.d/init/dired-cfg.el
+++ .emacs.d/init/dired-cfg.el
@@ -29,11 +29,11 @@
 
   (require 'dired-rainbow)
   (dired-rainbow-define audio      "#66aa66" ("mp3" "MP3" "ogg" "OGG" "flac" "FLAC" "wav" "WAV"))
-  (dired-rainbow-define video      "#ffdb1a" ("webm" "WEBM" "mkv" "MKV" "mpg" "MPG" "mp4" "MP4" "avi" "AVI" "mov" "MOV" "wmv" "m2v" "m4v" "mpeg" "MPEG" "flv" "FLV"))
+  (dired-rainbow-define video      "#c0c674" ("webm" "WEBM" "mkv" "MKV" "mpg" "MPG" "mp4" "MP4" "avi" "AVI" "mov" "MOV" "wmv" "m2v" "m4v" "mpeg" "MPEG" "flv" "FLV"))
   (dired-rainbow-define image      "#c97449" ("jpg" "JPG" "bmp" "BMP" "png" "PNG"))
-  (dired-rainbow-define archive    "#446699" ("zip" "ZIP" "tar" "TAR" "gz" "GZ" "rar" "RAR" "7z" "7Z"))
+  (dired-rainbow-define archive    "#a374a8" ("zip" "ZIP" "tar" "TAR" "gz" "GZ" "rar" "RAR" "7z" "7Z"))
   (dired-rainbow-define executable "#cc6666" ("py" "PY" "el" "EL" "hs" "HS" "sh" "SH"))
-  (dired-rainbow-define-chmod executable-unix "cc6666" "-.*x.*"))
+  (dired-rainbow-define-chmod executable-unix "#cc6666" "-.*x.*"))
 
 (provide 'dired-cfg)
 ;;; dired-cfg.el ends here
