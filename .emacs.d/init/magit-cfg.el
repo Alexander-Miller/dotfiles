905985f99e08719fc01ebf5e71afbe38b63f785f
a <alexanderm@web.de>
2015-08-14 17:39:48 +0200

    Rework config for magit v2.

--- .emacs.d/init/magit-cfg.el
+++ .emacs.d/init/magit-cfg.el
@@ -3,37 +3,44 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq-default magit-last-seen-setup-instructions "1.4.0")
-
 (with-eval-after-load 'magit
 
   (setq-default
    magit-highlight-trailing-whitespace t
+   magit-diff-show-lines-boundary      t
    magit-key-mode-show-usage           t
    magit-revert-backup                 t
    magit-revert-item-confirm           t
    magit-stage-all-confirm             t
    magit-use-overlays                  t)
 
+  (evil-set-initial-state 'magit-popup-mode 'emacs)
+
   (define-key magit-status-mode-map (kbd "C-v") 'set-mark-command)
 
-  (my/def-key-for-maps
-   (kbd "j") 'next-line (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
-  (my/def-key-for-maps
-   (kbd "k") 'previous-line (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
+  (defvar my/magit-key-maps
+    (list
+     magit-status-mode-map
+     magit-log-mode-map
+     magit-diff-mode-map
+     magit-branch-section-map
+     magit-untracked-section-map
+     magit-file-section-map
+     magit-status-mode-map
+     magit-hunk-section-map
+     magit-staged-section-map
+     magit-unstaged-section-map))
 
-  (my/def-key-for-maps
-   (kbd "J") 'my/quick-forward (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
-  (my/def-key-for-maps
-   (kbd "K") 'my/quick-backward (list magit-status-mode-map magit-log-mode-map magit-diff-mode-map magit-commit-mode-map))
+  (my/def-key-for-maps (kbd "j") 'next-line     my/magit-key-maps)
+  (my/def-key-for-maps (kbd "k") 'previous-line my/magit-key-maps)
 
-  (my/def-key-for-maps
-   (kbd "C-k") 'magit-discard-item (list magit-status-mode-map))
+  (my/def-key-for-maps (kbd "J") 'my/quick-forward  my/magit-key-maps)
+  (my/def-key-for-maps (kbd "K") 'my/quick-backward my/magit-key-maps)
 
-  (my/def-key-for-maps
-   (kbd "M-j") 'magit-goto-next-sibling-section (list magit-status-mode-map magit-diff-mode-map))
-  (my/def-key-for-maps
-   (kbd "M-k") 'magit-goto-previous-sibling-section (list magit-status-mode-map magit-diff-mode-map)))
+  (my/def-key-for-maps (kbd "C-k") 'magit-discard my/magit-key-maps)
+
+  (my/def-key-for-maps (kbd "M-j") 'magit-goto-next-sibling-section my/magit-key-maps)
+  (my/def-key-for-maps (kbd "M-k") 'magit-goto-previous-sibling-section my/magit-key-maps))
 
 (provide 'magit-cfg)
 ;;; magit-cfg.el ends here
