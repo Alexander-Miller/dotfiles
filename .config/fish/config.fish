74bd2f71e01c3cd41adea6d2ee56c053bfb028c2
Alexander Miller <alexanderm@web.de>
2015-09-27 22:52:19 +0200

    Refactoring.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,27 @@
-set -xU RUST_SRC_PATH $HOME/Documents/git/rust/src
-
-# remove ansi term prompt issues
-function fish_title;end
 alias ew='emacsclient -nw -a=""'
-alias sp='systemctl suspend'
-alias yt='youtube-dl'
 
-set -gx TERMINAL urxvt
+set -xU RUST_SRC_PATH $HOME/Documents/git/rust/src
+set -xg fish_color_search_match --background=black --foreground=blue
+set -xg fish_color_command blue
 
-. ~/.config/fish/local.fish
+function fish_title;end
+
+function yt
+    youtube-dl $argv
+end
+
+function ytmp3
+    yt --extract-audio --audio-format mp3 $argv
+end
+
+function fishconf
+    fish $HOME/.config/fish/config.fish
+end
+
+function setbg
+    feh --bg-scale $argv
+end
+
+function sp
+    systemctl suspend
+end
