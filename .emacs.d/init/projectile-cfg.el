1370a0753162aa847ba960ef993ac3313ae3eccc
Alexander Miller <alexanderm@web.de>
2015-09-29 17:17:26 +0200

    Remove with eval after load, shorten mode-line indicator.

--- .emacs.d/init/projectile-cfg.el
+++ .emacs.d/init/projectile-cfg.el
@@ -5,30 +5,29 @@
 
 (projectile-global-mode t)
 
-(with-eval-after-load "projectile"
+(helm-projectile-on)
 
-  (helm-projectile-on)
-  (setq-default projectile-completion-system 'helm)
+(setq-default
+ projectile-completion-system 'helm
+ projectile-mode-line '(:eval (format " P[%s]" (projectile-project-name))))
 
-  (evil-leader/set-key
-    "p f"   'helm-projectile-find-file
-    "p F"   'helm-projectile-find-file-in-known-projects
-    "p a"   'helm-projectile-ag
-    "p d"   'projectile-dired
-    "p C-D" 'helm-projectile-find-dir
-    "p l"   'my/helm-projectile-switch-to-buffer
-    "p p"   'helm-projectile-switch-project
-    "p g"   'projectile-vc
-    "p C-C" 'projectile-compile-project
-    "p C-T" 'projectile-test-project
-    "p C-R" 'projectile-run-shell-command-in-root
-    "p r"   'projectile-replace
-    "p t f" 'projectile-find-tag
-    "p t r" 'projectile-regenerate-tags
-    "p c f" 'projectile-cache-current-file
-    "p c i" 'projectile-invalidate-cache
-    )
-  )
+(evil-leader/set-key
+  "p f"   'helm-projectile-find-file
+  "p F"   'helm-projectile-find-file-in-known-projects
+  "p a"   'helm-projectile-ag
+  "p d"   'projectile-dired
+  "p C-D" 'helm-projectile-find-dir
+  "p l"   'my/helm-projectile-switch-to-buffer
+  "p p"   'helm-projectile-switch-project
+  "p g"   'projectile-vc
+  "p C-C" 'projectile-compile-project
+  "p C-T" 'projectile-test-project
+  "p C-R" 'projectile-run-shell-command-in-root
+  "p r"   'projectile-replace
+  "p t f" 'projectile-find-tag
+  "p t r" 'projectile-regenerate-tags
+  "p c f" 'projectile-cache-current-file
+  "p c i" 'projectile-invalidate-cache)
 
 (provide 'projectile-cfg)
 ;;; projectile-cfg.el ends here
