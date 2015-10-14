737b8cc457d5f93505a983a70286a907871bacdb
Alexander Miller <alexanderm@web.de>
2015-10-14 17:40:05 +0200

    New prompt.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,88 +1,91 @@
-function fish_prompt
-	and set retc green; or set retc red
-    tty|grep -q tty; and set tty tty; or set tty pts
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
 
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n .-
-    else
-        echo -n '┬─'
-    end
-    #set_color -o green
-    #echo -n [
-    if [ $USER = root ]
-        set_color -o red
-    else
-        set_color -o yellow
-    end
-    echo -n $USER
-    set_color -o white
-    echo -n @
-    if [ -z "$SSH_CLIENT" ]
-        set_color -o blue
-    else
-        set_color -o cyan
-    end
-    echo -n (hostname)
-    set_color -o yellow
-    #echo -n :(prompt_pwd)
-    echo -n :(pwd|sed "s=$HOME=~=")
-    set_color -o green
-    echo -n -s (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim"
-    set_color normal
-    set_color $retc
-    #if [ $tty = tty ]
-    #    echo -n '-'
-    #else
-    #    echo -n '─'
-    #end
-    #set_color -o green
-    #echo -n '['
-    #set_color normal
-    #set_color $retc
-    #echo -n (date +%X)
-    #set_color -o green
-    #echo -n ]
+	# Just calculate this once, to save a few cycles when displaying the prompt
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	end
 
-    # Check if acpi exists
-    if not set -q __fish_nim_prompt_has_acpi
-    	if type acpi > /dev/null
-    		set -g __fish_nim_prompt_has_acpi ''
-    	else
-    		set -g __fish_nim_prompt_has_acpi '' # empty string
-    	end
-    end
+	set -l normal (set_color normal)
 
-    if test "$__fish_nim_prompt_has_acpi"
-		if [ (acpi -a 2> /dev/null | grep off) ]
-			echo -n '─['
-			set_color -o red
-			echo -n (acpi -b|cut -d' ' -f 4-)
-			set_color -o green
-			echo -n ']'
+	# Hack; fish_config only copies the fish_prompt function (see #736)
+	if not set -q -g __fish_classic_git_functions_defined
+		set -g __fish_classic_git_functions_defined
+
+		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
+		
+		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
+		
+		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
+
+		function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
+			if status --is-interactive
+				commandline -f repaint ^/dev/null
+			end
+		end
+
+		# initialize our new variables
+		if not set -q __fish_classic_git_prompt_initialized
+			set -qU fish_color_user; or set -U fish_color_user -o green
+			set -qU fish_color_host; or set -U fish_color_host -o cyan
+			set -qU fish_color_status; or set -U fish_color_status red
+			set -U __fish_classic_git_prompt_initialized
 		end
 	end
-    echo
-    set_color normal
-    for job in (jobs)
-        set_color $retc
-        if [ $tty = tty ]
-            echo -n '; '
-        else
-            echo -n '│ '
-        end
-        set_color brown
-        echo $job
-    end
-    set_color normal
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n "'-> "
-    else
-        echo -n '╰─> '
-    end
-    #set_color -o red
-    #echo -n '$ '
-    set_color normal
 
+	set -l color_cwd
+	set -l prefix
+	switch $USER
+	case root toor
+		if set -q fish_color_cwd_root
+			set color_cwd $fish_color_cwd_root
+		else
+			set color_cwd $fish_color_cwd
+		end
+		set suffix '#'
+	case '*'
+		set color_cwd $fish_color_cwd
+		set suffix '>'
+	end
+
+	set -l prompt_status
+	if test $last_status -ne 0
+		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
+	end
+
+	set -l mode_str
+	switch "$fish_key_bindings"
+	case '*_vi_*' '*_vi'
+		# possibly fish_vi_key_bindings, or custom key bindings
+		# that includes the name "vi"
+		set mode_str (
+			echo -n " "
+			switch $fish_bind_mode
+			case default
+				set_color --bold --background red white
+				echo -n "[N]"
+			case insert
+				set_color --bold green
+				echo -n "[I]"
+			case visual
+				set_color --bold magenta
+				echo -n "[V]"
+			end
+			set_color normal
+		)
+	end
+
+	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
 end
