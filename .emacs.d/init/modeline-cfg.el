e90857f161b09408920cc35554e45428b675c074
Alexander Miller <alexanderm@web.de>
2015-11-13 17:36:47 +0100

    Use workspace unicode numbers.

--- .emacs.d/init/modeline-cfg.el
+++ .emacs.d/init/modeline-cfg.el
@@ -11,12 +11,13 @@
 (spaceline-toggle-selection-info-on)
 
 (setq-default
- anzu-cons-mode-line-p           nil
- spaceline-highlight-face-func   'spaceline-highlight-face-evil-state
- spaceline-minor-modes-separator " "
- powerline-height                24
- powerline-default-separator     'wave
- powerline-buffer-size-suffix    t)
+ anzu-cons-mode-line-p               nil
+ spaceline-highlight-face-func       'spaceline-highlight-face-evil-state
+ spaceline-minor-modes-separator     " "
+ spaceline-workspace-numbers-unicode t
+ powerline-height                    24
+ powerline-default-separator         'wave
+ powerline-buffer-size-suffix        t)
 
 (provide 'modeline-cfg)
 ;;; modeline-cfg.el ends here
