44d475ef8b4b562453154f0215ae94e9718669e5
Alexander Miller <alexanderm@web.de>
2015-11-20 17:54:47 +0100

    Manually set xdg config home.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
+#!/usr/bin/fish
 
 set -x RUST_SRC_PATH $HOME/Documents/git/rust/src
 set -x __fish_git_prompt_show_informative_status 1
 set -x fish_color_search_match --background=black --foreground=blue
 set -x fish_color_command blue
 set -x fish_color_operator magenta
+set -x XDG_CONFIG_HOME $HOME/.config
 
 source $XDG_CONFIG_HOME/fish/aliases/misc.fish
 source $XDG_CONFIG_HOME/fish/aliases/git.fish
