6fc8835d30642abc6ceeaf6eb9896d32855ca8b7
Alexander Miller <alexanderm@web.de>
2015-11-22 21:46:16 +0100

    New evil states bar colors.

--- .emacs.d/init/evil-cfg.el
+++ .emacs.d/init/evil-cfg.el
@@ -17,30 +17,17 @@
           (lambda () (setq-local cursor-type '(hbar . 5))))
 
 (setq-default
- evil-default-state      'normal
- evil-auto-indent        0
- evil-shift-width        4
- evil-repeat-move-cursor 0
- evil-find-skip-newlines t)
-
-(defvar hl-line-was-on nil)
-
-(defun visual-entry-hook ()
-  "Evil visual state entry hook."
-  (if global-hl-line-mode
-      (progn
-        (setq-local hl-line-was-on t)
-        (setq-local global-hl-line-mode nil))))
-
-(defun visual-exit-hook ()
-  "Evil visual state exit hook."
-  (if hl-line-was-on
-      (progn
-        (setq-local hl-line-was-on nil)
-        (setq-local global-hl-line-mode t))))
-
-(add-hook 'evil-visual-state-entry-hook #'visual-entry-hook)
-(add-hook 'evil-visual-state-exit-hook  #'visual-exit-hook)
+ evil-normal-state-cursor   '("#ab3737" box)
+ evil-insert-state-cursor   '("#33aa33" bar)
+ evil-motion-state-cursor   '("#c97449" box)
+ evil-operator-state-cursor '("#cc6666" '(hbar . 5))
+ evil-emacs-state-cursor    '("#339999" bar)
+ evil-resize-state-cursor   '("#ffdb1a" box)
+ evil-default-state         'normal
+ evil-auto-indent           0
+ evil-shift-width           4
+ evil-repeat-move-cursor    0
+ evil-find-skip-newlines    t)
 
 (provide 'evil-cfg)
 ;;; evil-cfg.el ends here
