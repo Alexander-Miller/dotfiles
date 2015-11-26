6a2c2c04bab87263995c0bd09467ba5cfe8e0589
Alexander Miller <alexander.miller@novatec-gmbh.de>
2015-11-26 14:50:37 +0100

    Add doc string functionality, change syntax table to accomodate ~/.

--- .emacs.d/init/fish-cfg.el
+++ .emacs.d/init/fish-cfg.el
@@ -8,9 +8,8 @@
   (defvar shell-functions '())
 
   (defun create-shell-functions-list ()
-    (let ((path-funcs (get-path-functions))
-          (fish-funcs (get-fish-functions)))
-      (setq shell-functions (sort (delete-dups (append path-funcs fish-funcs)) 'string-lessp))))
+    (setq shell-functions
+          (sort (delete-dups (append (get-fish-functions) (get-path-functions))) 'string-lessp)))
 
   (defun get-fish-functions ()
     (split-string (shell-command-to-string "functions") "\n"))
@@ -24,12 +23,29 @@
     (when (null shell-functions) (create-shell-functions-list))
     shell-functions)
 
+  (defun get-doc-buffer (arg)
+    (company-doc-buffer
+     (let ((man-page (shell-command-to-string (format "man %s" arg))))
+       (if (or
+            (null man-page)
+            (string-empty-p man-page)
+            (string-prefix-p "No manual entry" man-page))
+           (or
+            (shell-command-to-string (format "%s --help" arg))
+            (shell-command-to-string (format "%s -h" arg)))
+         man-page))))
+
   (defun company-shell-functions (command &optional arg &rest ignored)
     (interactive (list 'interactive))
-    (cl-case command
-      (interactive (company-begin-backend 'company-sample-backend))
-      (prefix (company-grab-symbol))
-      (candidates
+    (pcase command
+      (`interactive (company-begin-backend 'company-shell-functions))
+      (`prefix (company-grab-symbol))
+      (`sorted t)
+      (`duplicates nil)
+      (`ignore-case nil)
+      (`doc-buffer (get-doc-buffer arg))
+      (`meta (nth 0 (split-string (shell-command-to-string (format "whatis %s" arg)) "\n")))
+      (`candidates
        (cl-remove-if-not
         (lambda (c) (string-prefix-p arg c))
         (fetch-shell-functions)))))
@@ -38,6 +54,8 @@
     (setq-local company-backends '((company-shell-functions company-dabbrev-code company-files) company-dabbrev)))
 
   (add-hook 'fish-mode-hook #'fish-hook)
+
+  (modify-syntax-entry ?~ "_" fish-mode-syntax-table)
 
   (define-key fish-mode-map (kbd "C-c C-c") #'executable-interpret)
   (define-key fish-mode-map (kbd "C-M-x")   #'sh-execute-region)
