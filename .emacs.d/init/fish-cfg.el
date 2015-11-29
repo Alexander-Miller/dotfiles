27cb2e4887df9af0af944af8b8a82b71ad77ce0d
Alexander Miller <alexanderm@web.de>
2015-11-29 15:18:50 +0100

    Add company-yasnippet, add '.' as symbol part.

--- .emacs.d/init/fish-cfg.el
+++ .emacs.d/init/fish-cfg.el
@@ -51,11 +51,12 @@
         (fetch-shell-functions)))))
 
   (defun fish-hook ()
-    (setq-local company-backends '((company-shell-functions company-dabbrev-code company-files) company-dabbrev)))
+    (setq-local company-backends '((company-shell-functions company-dabbrev-code company-files company-yasnippet))))
 
   (add-hook 'fish-mode-hook #'fish-hook)
 
   (modify-syntax-entry ?~ "_" fish-mode-syntax-table)
+  (modify-syntax-entry ?. "_" fish-mode-syntax-table)
 
   (define-key fish-mode-map (kbd "C-c C-c") #'executable-interpret)
   (define-key fish-mode-map (kbd "C-M-x")   #'sh-execute-region)
