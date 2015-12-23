69cdd54a52cb1cf8a180be63d54ec52a7ca697ff
Alexander Miller <alexanderm@web.de>
2015-12-23 15:55:16 +0100

    Use fine undo.

--- .emacs.d/init/evil-cfg.el
+++ .emacs.d/init/evil-cfg.el
@@ -17,17 +17,18 @@
           (lambda () (setq-local cursor-type '(hbar . 5))))
 
 (setq-default
+ evil-auto-indent           0
+ evil-default-state         'normal
+ evil-find-skip-newlines    t
+ evil-repeat-move-cursor    0
+ evil-shift-width           4
+ evil-want-fine-undo        t
  evil-normal-state-cursor   '("#ab3737" box)
  evil-insert-state-cursor   '("#33aa33" bar)
  evil-motion-state-cursor   '("#c97449" box)
  evil-operator-state-cursor '("#cc6666" '(hbar . 5))
  evil-emacs-state-cursor    '("#339999" bar)
- evil-resize-state-cursor   '("#ffdb1a" box)
- evil-default-state         'normal
- evil-auto-indent           0
- evil-shift-width           4
- evil-repeat-move-cursor    0
- evil-find-skip-newlines    t)
+ evil-resize-state-cursor   '("#ffdb1a" box))
 
 (evil-define-state resize
   "Evil Resize State"
