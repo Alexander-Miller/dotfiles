c739bab156dfdcc1fd37be6353945d110fd5e436
Alexander Miller <alexanderm@web.de>
2016-01-23 15:04:26 +0100

    Add anzu placeholder to avoid color switch.

--- .emacs.d/init/modeline-cfg.el
+++ .emacs.d/init/modeline-cfg.el
@@ -4,7 +4,6 @@
 ;;; Code:
 
 (require 'spaceline-config)
-(spaceline-spacemacs-theme)
 (spaceline-toggle-anzu-on)
 (spaceline-toggle-evil-state-on)
 (spaceline-toggle-buffer-modified-on)
@@ -18,6 +17,43 @@
  powerline-height                    24
  powerline-default-separator         'wave
  powerline-buffer-size-suffix        t)
+
+(spaceline-define-segment anzu-placeholder
+  "Empty space for when anzu is off."
+  "     "
+  :when (not (and active (bound-and-true-p anzu--state))))
+
+(spaceline-install
+
+   '(((workspace-number window-number)
+      :fallback evil-state
+      :separator "|"
+      :face highlight-face)
+     anzu
+     anzu-placeholder
+     (buffer-modified buffer-size buffer-id remote-host)
+     major-mode
+     ((flycheck-error flycheck-warning flycheck-info)
+      :when active)
+     (((minor-modes :separator spaceline-minor-modes-separator)
+       process)
+      :when active)
+     (erc-track :when active)
+     (version-control :when active)
+     (org-pomodoro :when active)
+     (org-clock :when active)
+     nyan-cat)
+
+   `((battery :when active)
+     selection-info
+     ((buffer-encoding-abbrev
+       point-position
+       line-column)
+      :separator " | ")
+     (global :when active)
+     ;; ,@additional-segments
+     buffer-position
+     hud))
 
 (provide 'modeline-cfg)
 ;;; modeline-cfg.el ends here
