92734fd46d7f7fd44f2ad57a0e3930e270afc32d
Alexander Miller <alexanderm@web.de>
2016-01-24 21:50:01 +0100

    Fix i3 conf mode name.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,7 +19,7 @@
 (require 'projectile-cfg)
 (require 'smartparens-cfg)
 (require 'fish-cfg)
-(require 'i3-conf-mode-cfg)
+(require 'i3-config-mode-cfg)
 (require 'keybinds-cfg)
 (require 'diminish-cfg)
 (require 'local-cfg)
