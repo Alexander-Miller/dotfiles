22dbb362f7b93a14f06d5833bbf273946b5b8ec1
Alexander Miller <alexanderm@web.de>
2016-01-25 18:15:47 +0100

    Reorder settings alphabetically.

--- .emacs.d/init/helm-cfg.el
+++ .emacs.d/init/helm-cfg.el
@@ -7,37 +7,36 @@
 (helm-flx-mode t)
 
 (setq-default
- helm-ag-base-command                   "ag -f --hidden --nocolor --nogroup --depth -1"
- helm-echo-input-in-header-line         t
- helm-M-x-fuzzy-match                   t
- helm-semantic-fuzzy-match              t
- helm-imenu-fuzzy-match                 t
+ helm-adaptive-mode                     t
+ helm-ag-base-command                   "ag -f --hidden --nocolor --nogroup --depth 999999"
  helm-apropos-fuzzy-match               t
- helm-buffers-fuzzy-matching            t
- helm-completion-in-region-fuzzy-match  t
- helm-file-cache-fuzzy-match            t
- helm-projectile-fuzzy-match            t
- helm-semantic-fuzzy-match              t
- helm-locate-fuzzy-match                nil
- helm-recentf-fuzzy-match               t
- helm-candidate-number-limit            100
- helm-move-to-line-cycle-in-source      t
- helm-scroll-amount                     5
- helm-ff-file-name-history-use-recentf  t
  helm-autoresize-mode                   t
  helm-autoresize-max-height             50
  helm-autoresize-min-height             50
- helm-adaptive-mode                     t
- helm-ff-search-library-in-sexp         t
- helm-quick-update                      t
- helm-idle-delay                        0.1
- helm-input-idle-delay                  0.1
- helm-display-source-at-screen-top      nil
+ helm-buffers-fuzzy-matching            t
+ helm-candidate-number-limit            100
+ helm-completion-in-region-fuzzy-match  t
  helm-completion-window-scroll-margin   5
- helm-split-window-default-side         'right
- helm-kill-ring-max-lines-number        5
- helm-ff-transformer-show-only-basename t
+ helm-display-source-at-screen-top      nil
+ helm-echo-input-in-header-line         t
  helm-ff-auto-update-initial-value      nil
+ helm-ff-file-name-history-use-recentf  t
+ helm-ff-search-library-in-sexp         t
+ helm-ff-transformer-show-only-basename t
+ helm-file-cache-fuzzy-match            t
+ helm-idle-delay                        0.1
+ helm-imenu-fuzzy-match                 t
+ helm-input-idle-delay                  0.1
+ helm-kill-ring-max-lines-number        5
+ helm-locate-fuzzy-match                nil
+ helm-M-x-fuzzy-match                   t
+ helm-move-to-line-cycle-in-source      t
+ helm-projectile-fuzzy-match            t
+ helm-quick-update                      t
+ helm-recentf-fuzzy-match               t
+ helm-scroll-amount                     5
+ helm-semantic-fuzzy-match              t
+ helm-split-window-default-side         'right
  helm-for-files-preferred-list          '(helm-source-buffers-list
                                           helm-source-recentf
                                           helm-source-bookmarks
