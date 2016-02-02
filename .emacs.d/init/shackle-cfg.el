77bf26bfc2a925597920c77afcda9c264fae406a
Alexander Miller <alexanderm@web.de>
2016-02-02 19:53:51 +0100

    Add compilation-mode & helm magit-status.

--- .emacs.d/init/shackle-cfg.el
+++ .emacs.d/init/shackle-cfg.el
@@ -7,15 +7,17 @@
 
 ;; Order matters - first more specific, then more general rules
 (setq-default shackle-rules
-              '((help-mode                 :select   t :align right :size 0.5)
-                (flycheck-error-list-mode  :noselect t :align below :size 0.33)
-                (magit-diff-mode           :noselect t :align right :size 0.5)
-                ("*helm-ag*"               :select   t :align right :size 0.5)
-                ("*helm semantic/imenu*"   :select   t :align right :size 0.4)
-                ("*Helm Find Files*"       :select   t :align below :size 0.3)
-                ("*Helm Completions*"      :select   t :align below :size 0.3)
-                ("*Helm file completions*" :select   t :align below :size 0.3)
-                (".*helm.*"                :regexp   t :align below :size 0.5)))
+              '((help-mode                  :select   t :align right :size 0.5)
+                (compilation-mode           :select   t :align right :size 0.5)
+                (flycheck-error-list-mode   :noselect t :align below :size 0.33)
+                (magit-diff-mode            :noselect t :align right :size 0.5)
+                ("*helm-ag*"                :select   t :align right :size 0.5)
+                ("*helm semantic/imenu*"    :select   t :align right :size 0.4)
+                ("*Helm Find Files*"        :select   t :align below :size 0.3)
+                ("*helm-mode-magit-status*" :select   t :align below :size 0.3)
+                ("*Helm Completions*"       :select   t :align below :size 0.3)
+                ("*Helm file completions*"  :select   t :align below :size 0.3)
+                (".*helm.*"                 :regexp   t :align below :size 0.5)))
 
 (provide 'shackle-cfg)
 ;; shackle-cfg.el ends here
