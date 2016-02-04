39e36ff29bd304d119d9368d1b5133869b3a84cd
Alexander Miller <alexanderm@web.de>
2016-02-04 16:56:17 +0100

    Steal spacemacs' fringe indicators.

--- .emacs.d/init/flycheck-cfg.el
+++ .emacs.d/init/flycheck-cfg.el
@@ -9,6 +9,40 @@
 
 (with-eval-after-load "flycheck"
 
+  (define-fringe-bitmap 'my-flycheck-fringe-indicator
+    (vector #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00011100
+            #b00111110
+            #b00111110
+            #b00111110
+            #b00011100
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000))
+
+  (flycheck-define-error-level 'error
+    :overlay-category 'flycheck-error-overlay
+    :fringe-bitmap 'my-flycheck-fringe-indicator
+    :fringe-face 'flycheck-fringe-error)
+
+  (flycheck-define-error-level 'warning
+    :overlay-category 'flycheck-warning-overlay
+    :fringe-bitmap 'my-flycheck-fringe-indicator
+    :fringe-face 'flycheck-fringe-warning)
+
+  (flycheck-define-error-level 'info
+    :overlay-category 'flycheck-info-overlay
+    :fringe-bitmap 'my-flycheck-fringe-indicator
+    :fringe-face 'flycheck-fringe-info)
+
   (add-hook 'flycheck-mode-hook (lambda () (flycheck-pos-tip-mode)))
 
   (setq-default
