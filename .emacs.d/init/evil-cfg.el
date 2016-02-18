0e63be3777867a78f6947091206f876cdb872bc9
Alexander Miller <alexanderm@web.de>
2016-02-18 06:49:25 +0100

    Remove obsolete hooks, allow cursor beyond eol.

--- .emacs.d/init/evil-cfg.el
+++ .emacs.d/init/evil-cfg.el
@@ -10,23 +10,19 @@
 (evil-mode 1)
 (global-evil-matchit-mode 1)
 
-(add-hook 'evil-emacs-state-entry-hook
-          (lambda () (setq-local cursor-type '(bar . 1))))
-
-(add-hook 'evil-operator-state-entry-hook
-          (lambda () (setq-local cursor-type '(hbar . 5))))
-
 (setq-default
  evil-auto-indent           0
  evil-default-state         'normal
  evil-find-skip-newlines    t
+ evil-move-cursor-back      t
+ evil-move-beyond-eol       t
  evil-repeat-move-cursor    0
  evil-shift-width           4
  evil-want-fine-undo        t
  evil-normal-state-cursor   '("#ab3737" box)
  evil-insert-state-cursor   '("#33aa33" bar)
  evil-motion-state-cursor   '("#c97449" box)
- evil-operator-state-cursor '("#cc6666" '(hbar . 5))
+ evil-operator-state-cursor '("#cc0000" (hbar . 5))
  evil-emacs-state-cursor    '("#339999" bar)
  evil-resize-state-cursor   '("#ffdb1a" box))
 
