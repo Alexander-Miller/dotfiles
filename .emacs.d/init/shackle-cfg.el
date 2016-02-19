ddeb6adfd15a0e8cbbd37e6cd8bbda6cd99e03f9
Alexander Miller <alexanderm@web.de>
2016-02-19 06:41:53 +0100

    Correct rule order.

--- .emacs.d/init/shackle-cfg.el
+++ .emacs.d/init/shackle-cfg.el
@@ -6,21 +6,21 @@
 (shackle-mode t)
 
 ;; Order matters - first more specific, then more general rules
-(setq-default shackle-rules
-              '((help-mode                  :select   t :align right :size 0.5)
-                (special-mode               :select   t :align right :size 0.5)
-                (compilation-mode           :select   t :align right :size 0.5)
-                (Man-mode                   :select   t :align right :size 0.5)
-                (flycheck-error-list-mode   :noselect t :align below :size 0.33)
-                (magit-diff-mode            :noselect t :align right :size 0.5)
-                ("*helm-ag*"                :select   t :align right :size 0.5)
-                ("*helm semantic/imenu*"    :select   t :align right :size 0.4)
-                ("*Helm Find Files*"        :select   t :align below :size 0.3)
-                ("*Helm man woman*"         :select   t :align below :size 0.3)
-                ("*helm-mode-magit-status*" :select   t :align below :size 0.3)
-                ("*Helm Completions*"       :select   t :align below :size 0.3)
-                ("*Helm file completions*"  :select   t :align below :size 0.3)
-                (".*helm.*"                 :regexp   t :align below :size 0.5)))
+(setq shackle-rules
+      '(("*helm-ag*"                :select   t :align right :size 0.5)
+        ("*helm semantic/imenu*"    :select   t :align right :size 0.4)
+        ("*Helm Find Files*"        :select   t :align below :size 0.3)
+        ("*Helm man woman*"         :select   t :align below :size 0.3)
+        ("*helm-mode-magit-status*" :select   t :align below :size 0.3)
+        ("*Helm Completions*"       :select   t :align below :size 0.3)
+        ("*Helm file completions*"  :select   t :align below :size 0.3)
+        (".*helm.*"                 :regexp   t :align below :size 0.5)
+        (flycheck-error-list-mode   :noselect t :align below :size 0.33)
+        (Man-mode                   :select   t :align right :size 0.5)
+        (magit-diff-mode            :noselect t :align right :size 0.5)
+        (compilation-mode           :select   t :align right :size 0.5)
+        (help-mode                  :select   t :align right :size 0.5)
+        (special-mode               :select   t :align right :size 0.5)))
 
 (provide 'shackle-cfg)
 ;; shackle-cfg.el ends here
