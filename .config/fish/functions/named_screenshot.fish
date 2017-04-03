9f1e465465208bbd11e1599f9a77ae08e27c4373
Alexander M <alexanderm@web.de>
2017-04-03 08:43:01 +0200

    Make sure a name was entered when taking screenshot.

--- .config/fish/functions/named_screenshot.fish
+++ .config/fish/functions/named_screenshot.fish
@@ -1,4 +1,6 @@
 function named_screenshot
-  set -l name (rofi -dmenu -lines 1 -columns 1 -line-margin 5 -padding 10 -p 'name: ')
-  maim -s ~/Pictures/$name.png
+    set -l name (rofi -dmenu -lines 1 -columns 1 -line-margin 5 -padding 10 -p 'name: ')
+    if test -n "$name"
+        maim -s ~/Pictures/$name.png
+    end
 end
