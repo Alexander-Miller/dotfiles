6e4e78eaee202b2336ee7cc76eb3cea172cf4578
Alexander Miller <alexanderm@web.de>
2019-03-27 21:22:47 +0100

    [Shell] Add icon to youtube-dl notifications.

--- .config/fish/functions/youtube-dl.fish
+++ .config/fish/functions/youtube-dl.fish
@@ -1,7 +1,7 @@
 function youtube-dl
   if command youtube-dl $argv
-    notify-send "Success" "youtube-dl has finished."
+    notify-send "Success" "youtube-dl has finished." --icon=youtube
   else
-    notify-send "Error" "youtube-dl has failed."
+    notify-send "Error" "youtube-dl has failed." --icon=youtube
   end
 end
