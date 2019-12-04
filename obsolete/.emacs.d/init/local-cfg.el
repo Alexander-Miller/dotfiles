f09ee325c52ad19b7be42b6d45241c1b30ac54c9
Alexander Miller <alexanderm@web.de>
2019-12-04 17:17:13 +0100

    [Misc] Delete obsolete files.

--- obsolete/.emacs.d/init/local-cfg.el
+++
@@ -1,6 +0,0 @@
-
-(load-file "/home/a/Documents/git/morning-star-theme/morning-star-theme.el")
-(custom-set-faces
- `(linum ((t (:height 120)))))
-
-(provide 'local-cfg)
--- obsolete/.emacs.d/recipes/morning-star-theme.el
+++
@@ -1,6 +0,0 @@
-(:name morning-star-theme
-       :description "Emacs morning star color theme."
-       :website "https://github.com/Alexander-Miller/morning-star-theme"
-       :type github
-       :pkgname "Alexander-Miller/morning-star-theme"
-       :prepare (add-to-list 'custom-theme-load-path default-directory))
