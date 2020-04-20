06ad47303511568dcb10f2b3a8a53e4b38a52dd4
Alexander Miller <alexanderm@web.de>
2020-04-20 16:33:53 +0200

    [i3] Send notifications when a screenshot was taken.

--- .config/fish/functions/named_screenshot.fish
+++ .config/fish/functions/named_screenshot.fish
@@ -2,5 +2,6 @@ function named_screenshot
     set -l name (rofi -dmenu -lines 1 -columns 1 -line-margin 5 -padding 10 -p 'name: ')
     if test -n "$name"
         maim -s ~/Pictures/$name.png
+        notify-send "Screenshot taken"
     end
 end
