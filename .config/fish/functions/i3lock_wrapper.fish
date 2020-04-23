ccf3aa8b937f75c6b0819c31c134631cf486ea26
Alexander Miller <alexanderm@web.de>
2020-04-23 12:40:26 +0200

    [i3] Lock screen with pixelation instead of liquid rescale.

--- .config/fish/functions/i3lock_wrapper.fish
+++ .config/fish/functions/i3lock_wrapper.fish
@@ -2,8 +2,31 @@
 
 function i3lock_wrapper --description "Sets a blurred screenshot of the current desktop as i3lock background."
     set tmpfile (mktemp --tmpdir i3lock-wrapper-XXXXXXXXXX.png)
+    set lock_img ~/.config/i3/i3lock.png
+
     maim $tmpfile
-    mogrify -liquid-rescale 25% -liquid-rescale 400% $tmpfile
+    convert $tmpfile -gamma 0.7 -scale 25% -scale 400% $tmpfile
+
+    set lock_img_size (file $lock_img | rg -o '[0-9]* x [0-9]*' | string split " x ")
+    set lock_img_size_x $lock_img_size[1]
+    set lock_img_size_y $lock_img_size[2]
+    set resolutions (xrandr --query | rg ' connected' | rg -o "[0-9]+x[0-9]+\+[0-9]+\+[0-9]+")
+
+    for resolution in $resolutions
+        set tokens (string split "+" $resolution)
+        set w_h    (string split "x" $tokens[1])
+        set width  $w_h[1]
+        set height $w_h[2]
+
+        set offset_x $tokens[2]
+        set offset_y $tokens[3]
+
+        set center_x (math "$offset_x + $width/2 - $lock_img_size_x/2")
+        set center_y (math "$offset_y + $height/2 - $lock_img_size_y/2")
+
+        convert $tmpfile $lock_img -geometry +$center_x+$center_y -composite -matte $tmpfile
+    end
+
     i3lock -i $tmpfile
     rm $tmpfile
 end
