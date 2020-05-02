173433fcdafb0cf087cc254098658bf7c57c5413
Alexander Miller <alexanderm@web.de>
2020-05-02 16:03:44 +0200

    [LSP] Set read-process-output-max to 1mb.

--- .config/emacs/modules/lsp.el
+++ .config/emacs/modules/lsp.el
@@ -6,4 +6,6 @@
  lsp-treemacs)
 
 (std::after lsp-mode
-  (setf lsp-ui-flycheck-live-reporting nil))
+  (setf lsp-ui-flycheck-live-reporting nil
+        lsp-prefer-capf                t
+        read-process-output-max        (* 1024 1024)))
