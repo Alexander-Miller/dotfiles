3d590ffdc391f16445d33a679a3a22214176ae32
Alexander Miller <alexanderm@web.de>
2020-07-14 09:46:19 +0200

    [Search] Use goto-char without timer.

--- .config/emacs/modules/search.el
+++ .config/emacs/modules/search.el
@@ -21,7 +21,7 @@
   "C-s"   #'swiper
   "C-M-S" #'swiper-thing-at-point
   "M-o"   #'evil-avy-goto-char-2
-  "M-O"   #'evil-avy-goto-char-timer
+  "M-O"   #'evil-avy-goto-char
   :leader
   "/"  #'std::search::rg-in-project
   "sf" #'helm-do-ag-this-file
