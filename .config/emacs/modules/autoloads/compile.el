c00fc686798075b6d52243e3df8d8ac7e80efe3b
Alexander Miller <alexanderm@web.de>
2020-07-22 18:07:23 +0200

    [Compile] Detect and set number of cores when running make.

--- .config/emacs/modules/autoloads/compile.el
+++ .config/emacs/modules/autoloads/compile.el
@@ -12,22 +12,28 @@
 (defun std::multi-compile ()
   (interactive)
   (let* ((default-directory
-           (or (projectile-project-root)
+           (or (projectile-project-root default-directory)
                default-directory))
         (multi-compile-alist
          (append multi-compile-alist
                  (std::compile::get-make-targets))))
-    (call-interactively #'multi-compile-run)))
+    (multi-compile-run)))
 
 (defun std::compile::get-make-targets ()
   (when (file-exists-p "Makefile")
-    (list
-     (cons
-      "\\.*"
-      (->> "cat Makefile | rg .PHONY: | cut -d ' ' -f 1-999"
-           (shell-command-to-string)
-           (s-trim-right)
-           (s-split " ")
-           (cdr)
-           (--reject (s-starts-with? "." it))
-           (--map (cons (capitalize it) (format "make %s" it))))))))
+    (-let [cores (or
+                  (when (file-exists-p "/proc/cpuinfo")
+                    (with-temp-buffer
+                      (insert-file-contents "/proc/cpuinfo")
+                      (how-many "^processor[[:space:]]+:")))
+                  1)]
+      (list
+       (cons
+        "\\.*"
+        (->> "cat Makefile | rg .PHONY: | cut -d ' ' -f 1-999"
+             (shell-command-to-string)
+             (s-trim-right)
+             (s-split " ")
+             (cdr)
+             (--reject (s-starts-with? "." it))
+             (--map (cons (capitalize it) (format "make %s -j%s" it cores)))))))))
