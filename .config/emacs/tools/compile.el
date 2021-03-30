a0c8117d13e1d11f63bbca7cf2e3af468f7cf0d2
Alexander Miller <alexanderm@web.de>
2021-03-30 22:48:32 +0200

    [EM] Don't catch exceptions when compiler, show full stack trace.

--- .config/emacs/tools/compile.el
+++ .config/emacs/tools/compile.el
@@ -2,6 +2,8 @@
 
 (require 'cl-lib)
 
+(setq-default debug-on-error t)
+
 (load (concat (getenv "EMACS_HOME") "tools/logging.el") nil :no-message)
 
 ;; force file compilation with output and signal an `error'
@@ -19,41 +21,35 @@
 (defun std::load (file)
   (load file nil :no-message))
 
-;; wrapper to catch and log all errors
-(defmacro std::run (&rest body)
-  `(condition-case e
-       (progn ,@body)
-     (error
-      (std::err (format "Got Error %s" (error-message-string e)))
-      (std::err "Done With Error")
-      (kill-emacs 1))))
+(std::log "Compilation Process Started")
 
-(std::run
+(std::log "Delete previous byte code")
+(dolist (elc-file (directory-files-recursively
+                   (concat (getenv "EMACS_HOME") "modules") (rx ".elc")))
+  (delete-file elc-file :trash))
 
- (std::log "Compilation Process Started")
+(std::log "Load Init File")
+(std::load "~/.emacs.d/init.el")
 
- (std::log "Delete previous byte code")
- (dolist (elc-file (directory-files-recursively
-                    (concat (getenv "EMACS_HOME") "modules") (rx ".elc")))
-   (delete-file elc-file :trash))
+(let* ((byte-compile-warnings '(not unresolved free-vars make-local)))
 
- (std::log "Load Init File")
- (std::load "~/.emacs.d/init.el")
+  (std::log "Compiling Modules")
 
- (let* ((byte-compile-warnings '(not unresolved free-vars make-local)))
+  (dolist (file (std::files std::module-dir))
+    (when (string= "el" (file-name-extension file))
+      (when (or (null filter)
+                (--any? (s-contains? it file) filter))
+        (std::log (format "  Compile %s" (file-name-nondirectory file)))
+        (std::compile file)
+        (std::clear-line))))
 
-   (std::log "Compiling Modules")
-   (dolist (file (std::files std::module-dir))
-     (when (string= "el" (file-name-extension file))
-       (std::log (format "  Compile %s" (file-name-nondirectory file)))
-       (std::compile file)
-       (std::clear-line)))
+  (std::log "Compiling Autoloads")
+  (dolist (file (std::files std::autoloads-dir))
+    (when (string= "el" (file-name-extension file))
+      (when (or (null filter)
+                (--any? (s-contains? it file) filter))
+        (std::log (format "  Compile %s" (file-name-nondirectory file)))
+        (std::compile file)
+        (std::clear-line)))))
 
-   (std::log "Compiling Autoloads")
-   (dolist (file (std::files std::autoloads-dir))
-     (when (string= "el" (file-name-extension file))
-       (std::log (format "  Compile %s" (file-name-nondirectory file)))
-       (std::compile file)
-       (std::clear-line))))
-
- (std::log "Compilation Complete"))
+(std::log "Compilation Complete")
