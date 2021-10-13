e151ab1788ecac2d6adc323879653bef9d0365c2
Alexander Miller <alexander.miller@novatec-gmbh.de>
2021-10-13 16:44:54 +0200

    [Fish] Fix `l` function.

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,7 +1,8 @@
 function l
-  if which exa > /dev/null ^&1
-    exa  $argv
-  else
-    ls -h $argv
-  end
+    set -l has_exa (which exa) &> /dev/null
+    if $has_exa
+        exa  $argv
+    else
+        ls -h $argv
+    end
 end
