af89d9027a59183947bef6414ac22e23851edb76
Alexander Miller <alexanderm@web.de>
2022-09-10 10:43:56 +0200

    [EM] Add command to open magit in the terminal.

--- .config/fish/functions/em.fish
+++ .config/fish/functions/em.fish
@@ -6,6 +6,13 @@ function em
 	  emacs -q --batch -l $EMACS_HOME/tools/sync.el $argv[2..-1]
 	case "prune"
 	  emacs -q --batch -l $EMACS_HOME/tools/prune.el $argv[2..-1]
+    case "git" "magit"
+      if git status > /dev/null 2>&1
+        emacsclient -nw --eval "(call-interactively #'magit-status)"
+      else
+        echo "Not in a git repo"
+        return 1
+      end
     case 'e'
       if test -e $argv[2]
         emacsclient --no-wait --create-frame $argv[2]
--- .config/fish/functions/magit.fish
+++
@@ -1,8 +0,0 @@
-function magit
-    if git status > /dev/null 2>&1
-        emacsclient -nw --eval "(call-interactively #'magit-status)"
-    else
-        echo "Not in a git repo"
-        return 1
-    end
-end
