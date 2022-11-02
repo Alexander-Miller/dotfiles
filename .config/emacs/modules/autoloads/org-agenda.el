66b25ae48d6a1965f0a6420a0ea30311377154cb
Alexander Miller <alexanderm@web.de>
2022-11-02 08:11:12 +0100

    [Org] Fix roam tag selection.

--- .config/emacs/modules/autoloads/org-agenda.el
+++ .config/emacs/modules/autoloads/org-agenda.el
@@ -122,31 +122,22 @@
 
 (cl-defun std::org::agenda::roam-files-with-tags (&key in not-in)
   (require 'org-roam)
-  (-map
-   #'car
-   (org-roam-db-query
-    (vector :select :distinct 'nodes:file
-            :from 'nodes
-            :where
-            (cond
-             ((and in not-in)
-              `(and
-                (:exists
-                 ,(vector :select 1 :from 'tags :where
-                          `(and (in tag ,(apply #'vector in))
-                                (= tags:node-id nodes:id))))
-                (:exists
-                 ,(vector :select 1 :from 'tags :where
-                          `(and (not (in tag ,(apply #'vector not-in)))
-                                (= tags:node-id nodes:id))))))
-             (in
-              `(:exists
-                ,(vector :select 1 :from 'tags :where
-                         `(and (in tag ,(apply #'vector in))
-                               (= tags:node-id nodes:id)))))
-             (not-in
-              `(:exists
-                ,(vector :select 1 :from 'tags :where
-                         `(and (not (in tag ,(apply #'vector not-in)))
-                               (= tags:node-id nodes:id)))))
-             (t (error "")))))))
+  (let ((in-clause
+         (format "1 = (SELECT COUNT (*) FROM (SELECT tag FROM tags WHERE tags.node_id = nodes.id AND tags.tag IN (%s)))"
+                 (string-join
+                  (--map (format "'\"%s\"'" it) in) ", ")))
+        (not-in-clause
+         (format "0 = (SELECT COUNT (*) FROM (SELECT tag FROM tags WHERE tags.node_id = nodes.id AND tags.tag IN (%s)))"
+                 (string-join
+                  (--map (format "'\"%s\"'" it) not-in) ", "))))
+    (-map
+     #'car
+     (org-roam-db-query
+      (format
+       "SELECT nodes.file FROM nodes WHERE %s"
+       (cond
+        ((and in not-in)
+         (format "%s AND %s" in-clause not-in-clause))
+        (in in-clause)
+        (not-in not-in-clause)
+        (t (error "Neither in nor not-in predicates are defined"))))))))
