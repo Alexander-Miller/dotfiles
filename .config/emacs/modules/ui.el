795cfb1d289accb581d2babf9ae6162bd762f299
Alexander Miller <alexanderm@web.de>
2023-05-22 22:20:41 +0200

    [UI] Add missing autoload.

--- .config/emacs/modules/ui.el
+++ .config/emacs/modules/ui.el
@@ -11,7 +11,8 @@
 
 (std::autoload ui
   #'std::ui::writeroom-hide-line-numbers
-  #'std::ui::hydra/body)
+  #'std::ui::hydra/body
+  #'std::ui::reload-theme)
 
 (defvar std::ui::default-font-spec
   `(:family "Fantasque Sans Mono" :size ,(std::if-work-laptop 22 16)))
