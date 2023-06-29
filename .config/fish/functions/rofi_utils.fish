c1b389878d140ff9a73429fd46675136046aac3e
Alexander Miller <alexanderm@web.de>
2023-06-29 07:43:10 +0200

    [Fish] Make rofi utils case-insensitive.

--- .config/fish/functions/rofi_utils.fish
+++ .config/fish/functions/rofi_utils.fish
@@ -3,29 +3,29 @@ function rofi_utils
   set -l pick_book "Pick Book"
   set -l copy_clipboard "Copy From Clipboard History"
   set -l ytmpv "Watch Video With MPV"
-  set -l choice (echo -n $change_wallpaper\n$pick_book\n$copy_clipboard\n$ytmpv | rofi -dmenu)
+  set -l choice (echo -n $change_wallpaper\n$pick_book\n$copy_clipboard\n$ytmpv | rofi -dmenu -i)
 
   switch $choice
     case $change_wallpaper
       set -l wallpaper_dir "$HOME/Documents/git/dotfiles/.wallpapers"
-      set -l wallpaper (fd '' $wallpaper_dir -x echo {/} | rofi -dmenu)
+      set -l wallpaper (fd '' $wallpaper_dir -x echo {/} | rofi -dmenu -i)
       if test -n "$wallpaper"
         feh --bg-scale "$wallpaper_dir/$wallpaper"
       end
 
     case $pick_book
       set -l books_dir $HOME/Dropbox/Books
-      set -l book (fd pdf $books_dir -x echo {/} | rofi -dmenu)
+      set -l book (fd pdf $books_dir -x echo {/} | rofi -dmenu -i)
       if test -n "$book"
         zathura "$books_dir/$book" &
       end
 
     case $copy_clipboard
-      history | rofi -dmenu | xargs echo -n | xclip -sel clip
+      history | rofi -dmenu -i | xargs echo -n | xclip -sel clip
 
     case "$ytmpv"
       set -l url (xclip -selection c -o)
-      set -l resolution (echo -e "480\n720\n1080\nbest" | rofi -dmenu  -columns 2 -line-margin 5 -padding 10 -p 'Resolution: ')
+      set -l resolution (echo -e "480\n720\n1080\nbest" | rofi -dmenu -i -columns 2 -line-margin 5 -padding 10 -p 'Resolution: ')
       switch $resolution
         case "best"
           mpv --ytdl-format="best" $url
