429a0bbd7b35159806e6075850f05109e072c300
Alexander Miller <alexanderm@web.de>
2023-11-20 08:10:34 +0100

    [VCS] Include tags when fetching with magit.

--- .config/emacs/transient/transient-values.el
+++ .config/emacs/transient/transient-values.el
@@ -1,4 +1,4 @@
 ((magit-log:magit-log-mode "-n256" "--graph" "--color" "--decorate")
- (magit-fetch "--prune")
+ (magit-fetch "--prune" "--tags" "--force")
  (magit-stash "--include-untracked")
  (magit-rebase "--autostash"))
