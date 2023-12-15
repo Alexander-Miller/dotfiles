5713d978c2481caf272eebd2534dab1b12d96af4
Alexander Miller <alexander.miller@novatec-gmbh.de>
2023-12-15 18:05:43 +0100

    [Picom] Use systemd to run picom.

--- .config/fish/functions/on_i3_restart.fish
+++ .config/fish/functions/on_i3_restart.fish
@@ -1,9 +1,7 @@
 function on_i3_restart
     setxkbmap -option ctrl:nocaps
     killall xcape
-    killall picom
     xcape
-    picom &
     python ~/.config/polybar/run_polybar.py
     random_wallpaper
 end
