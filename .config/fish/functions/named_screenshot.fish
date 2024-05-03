af96a99bd7bd6cb0e6fcf311b434ad2f0e7e6828
Alexander Miller <alexander.miller@novatec-gmbh.de>
2024-05-03 12:31:42 +0200

    [Fish] Add fallback name for named screenshot command.

--- .config/fish/functions/named_screenshot.fish
+++ .config/fish/functions/named_screenshot.fish
@@ -1,7 +1,10 @@
 function named_screenshot
     set -l name (rofi -dmenu -lines 1 -columns 1 -line-margin 5 -padding 10 -p 'name: ')
-    if test -n "$name"
-        maim -s ~/Pictures/$name.png
-        notify-send "Screenshot '$name' taken" --icon="image"
+
+    if test -z $name
+      set name "screenshot-$(date +%F-%T)"
     end
+
+    maim -s ~/Pictures/$name.png
+    notify-send "Screenshot '$name' taken" --icon="image"
 end
