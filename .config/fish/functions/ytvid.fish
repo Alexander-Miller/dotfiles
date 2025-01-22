351869938f11f99376680c1ca3858f66c052efe8
Alexander Miller <alexanderm@web.de>
2025-01-22 22:42:05 +0100

    [Fish] Improve ytvid format detection.

--- .config/fish/functions/ytvid.fish
+++ .config/fish/functions/ytvid.fish
@@ -1,7 +1,11 @@
 function ytvid -w yt-dlp -a url video audio
   if test ! $video
-    yt-dlp --no-playlist -F "$url" | rg "60fps|audio"
+    yt-dlp --no-playlist -F "$url" | rg "60fps|audio|1080|720|HD"
   else
-    yt-dlp -o "%(title)s.%(ext)s" --no-playlist -f "$video"+"$audio" "$url"
+    if test ! $audio
+      yt-dlp -o "%(title)s.%(ext)s" --no-playlist -f "$video" "$url"
+    else
+      yt-dlp -o "%(title)s.%(ext)s" --no-playlist -f "$video"+"$audio" "$url"
+    end
   end
 end
