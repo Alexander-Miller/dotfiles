0d869e2e462c97e8b6f598254507415a4a85e33b
Alexander Miller <alexanderm@web.de>
2026-01-23 00:00:31 +0100

    [Stdlib] Add homeserver to private laptop definition.

--- .config/emacs/modules/stdlib.el
+++ .config/emacs/modules/stdlib.el
@@ -163,7 +163,7 @@ BODY can either be raw lambda body or a function reference."
      ,else))
 
 (defmacro std::if-private-laptop (&rest then)
-  `(when (string= "am-laptop" (system-name))
+  `(when (member (system-name) (list "homeserver" "am-laptop"))
      ,@then))
 
 (cl-defmacro std::notify (title &key (txt "") (icon :NONE))
